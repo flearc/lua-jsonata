51ea030891fedb3bc74adf92fe7dcf689a1c9720
fl <fleashiro@gmail.com>
2026-06-08 23:21:49 +0800

    style(errors): sort MESSAGES by code (S/T/D groups, ascending)

--- src/jsonata/errors.lua
+++ src/jsonata/errors.lua
@@ -4,18 +4,21 @@ local ERROR_MT = { __name = "jsonata.error" }
 
 -- Message templates for the M1 starter subset. Extended in later milestones.
 local MESSAGES = {
+  -- Syntax errors
   S0201 = "Syntax error",
   S0203 = "Expected token before end of expression",
   S0211 = "The symbol cannot be used as a unary operator",
+  -- Type errors
   T0410 = "Argument of function does not match function signature",
   T0412 = "Argument of function must be an array of strings",
-  D3020 = "Third argument of function must be a positive integer",
   T1006 = "Attempted to invoke a non-function",
   T2001 = "The left side of an operator must evaluate to a number",
   T2002 = "The right side of an operator must evaluate to a number",
   T2010 = "Operands of comparison must both be numbers or both be strings",
-  D3001 = "Unsupported in M1",
+  -- Dynamic / runtime errors
   D1001 = "Number out of range to be formatted",
+  D3001 = "Unsupported in M1",
+  D3020 = "Third argument of function must be a positive integer",
   D3030 = "Unable to cast value to a number",
   D3047 = "Argument of aggregate function must be an array of numbers",
   D3060 = "$sqrt of a number that is less than zero",
